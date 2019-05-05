e0eb4eb93947b6bcb6e700ab72d04befad649bf0
Ahmed Essam <theartful.ae@gmail.com>
2019-05-05 23:43:58 +0200

    Fix alu mov

--- src/execute_stage/alu/alu.vhd
+++ src/execute_stage/alu/alu.vhd
@@ -28,12 +28,13 @@ begin
 	b_tmp <= unsigned('0' & b);
 	s_tmp <=	a_tmp + b_tmp	when 	fun = alu_add else
 				a_tmp - b_tmp	when	fun = alu_sub else
-				a_tmp + 1		when	fun = alu_inc else
-				a_tmp - 1		when	fun = alu_dec else
+				b_tmp + 1		when	fun = alu_inc else
+				b_tmp - 1		when	fun = alu_dec else
+				not(b_tmp)		when	fun = alu_not else
 				a_tmp and b_tmp	when	fun = alu_and else
 				a_tmp or b_tmp	when	fun = alu_or  else
 				a_tmp 			when	fun = alu_first_op else
-				not(a_tmp)		when	fun = alu_not else
+				b_tmp 			when	fun = alu_second_op else
 				shift_left(a_tmp, to_integer(shift_tmp)) when fun = alu_shl else
 				shift_right(a_tmp, to_integer(shift_tmp)) when fun = alu_shr;
 
